b08739aabd7e4d02dc80bf7e2c9e6c2979167c01
reverseon <reverseon@outlook.com>
2021-11-12 00:15:12 +0700

    Repair some Bug + Additional Features

--- src/process.js
+++ src/process.js
@@ -5,6 +5,11 @@ const before_img = document.querySelector('#before_img');
 const isTruePixel = document.querySelector('#isTruePixel');
 const truepx_img = document.querySelector('#truePxImg');
 const downloadbutton = document.querySelector('.dlbtn');
+const perfstats = document.querySelector('.perfstats');
+let filename;
+let filetype;
+let startTime;
+let endTime;
 let aftercanvas = document.querySelector('#after_img');
 aftercanvas.width = 0;
 aftercanvas.height = 0;
@@ -14,10 +19,16 @@ const gpu = new GPU();
 let beforeSrc = '';
 prevSvdMatrices = [];
 
+isTruePixel.addEventListener('change', function(e) {
+    prevSvdMatrices = [];
+})
+
 function dlCanvas() {
     let linkdl = document.createElement('a');
-    linkdl.download = 'compressed.png';
-    linkdl.href = aftercanvas.toDataURL('image/png');
+    let origfilename = filename.substring(0, filename.lastIndexOf("."));
+    let origext = filename.substring(filename.lastIndexOf(".") + 1, filename.length);
+    linkdl.download = `${origfilename}_compressed.${origext}`;
+    linkdl.href = aftercanvas.toDataURL(filetype);
     linkdl.click();
 }
 
@@ -28,8 +39,10 @@ function dlCanvasResized() {
     resizedcv.height = origheight;
     resizedctx.drawImage(aftercanvas, 0, 0, origwidth, origheight);
     let linkdl = document.createElement('a');
-    linkdl.download = 'compressed.png';
-    linkdl.href = resizedcv.toDataURL('image/png');
+    let origfilename = filename.substring(0, filename.lastIndexOf("."));
+    let origext = filename.substring(filename.lastIndexOf(".") + 1, filename.length);
+    linkdl.download = `${origfilename}_compressed.${origext}`;
+    linkdl.href = resizedcv.toDataURL(filetype);
     linkdl.click();
 }
 
@@ -46,6 +59,15 @@ function genDlButton() {
     <button type="button" id="downloadcanvas" class="btn btn-success float-start">Download</button>
     <button type="button" id="downloadcanvasresized" class="btn btn-primary float-end">Download Resized</button>
     `;
+}
+
+function displayPerfStats() {
+    perfstats.innerHTML = `
+    <hr/>
+    <p class="card-text" style="margin-bottom: 5px !important;"><small class="text-muted" id="performancetimestats">Time: ${Math.round((Math.round(endTime - startTime) / 1000) * 100)/100}s</small>
+    </p>
+    <p class="card-text"><small class="text-muted" id="compratestats">Compression Rate: ${101 - comprateinp.value}%</small></p>
+    `
 }
 
 downloadbutton.addEventListener('click', function(e) {
@@ -109,8 +131,16 @@ function multiplySvdToMat(svd, percent){
     let {u, q, v} = svd;
     let cols = svd.u.length;
     let rows = svd.v.length;
-    let level = rows < cols ? Math.round(rows * percent / 100) : Math.round(cols * percent / 100);
-
+    let zeroatfirst = 0;
+    for (let i = 0; i < q.length; i++) {
+        if (q[i] > 0) {
+            break;
+        } else {
+            zeroatfirst++;
+        }
+    }
+    let level = rows < cols ? Math.round((rows - zeroatfirst) * percent / 100) : Math.round((cols - zeroatfirst) * percent / 100);
+    level = level + zeroatfirst;
     for (let i = 0; i < level; i++) {
         let rowNow = [];
         for (let j = 0; j < level; j++) {
@@ -146,24 +176,29 @@ function multiplySvdToMat(svd, percent){
 }
 
 imageInp.addEventListener('change', (e) => {
+    filename = e.target.files[0].name;
+    filetype = e.target.files[0].type;
     beforeSrc = URL.createObjectURL(e.target.files[0]);
     prevSvdMatrices = [];
 },false);
 
 userDataForm.addEventListener('submit', function(e) {
     e.preventDefault();
-    before_img.src = beforeSrc;
-    before_img.cprate = comprateinp.value;
     if (isTruePixel.checked) {
         truepx_img.cprate = comprateinp.value;
         truepx_img.src = beforeSrc;
+        before_img.src = beforeSrc;
+    } else {
+        before_img.src = beforeSrc;
+        before_img.cprate = comprateinp.value;
     }
 }); 
 
 before_img.onload = function() {
-    origwidth = this.naturalWidth;
-    origheight = this.naturalHeight;
     if (!isTruePixel.checked) {
+        origwidth = this.naturalWidth;
+        origheight = this.naturalHeight;
+        startTime = performance.now();
         let src = cv.imread(before_img);
         let dst = new cv.Mat();
         let rgbaSrc = new cv.MatVector();
@@ -175,27 +210,33 @@ before_img.onload = function() {
         rgbaDst.push_back(rgbaSrc.get(3))
         cv.merge(rgbaDst, dst);
         cv.imshow('after_img', dst);
+        endTime = performance.now();
         src.delete();
         dst.delete();
+        displayPerfStats();
         genDlButton();
     }
 }
 
 truepx_img.onload = function() {
     if (isTruePixel.checked) {
+        startTime = performance.now();
         let src = cv.imread(truepx_img);
         let dst = new cv.Mat();
         let rgbaSrc = new cv.MatVector();
         let rgbaDst = new cv.MatVector();
         cv.split(src, rgbaSrc);
         for (let i = 0; i < 3; i++) {
-            rgbaDst.push_back(arrToMat(compressChannel(matToArr(rgbaSrc.get(i)), truepx_img.cprate, i)));
+            let out = compressChannel(matToArr(rgbaSrc.get(i)), truepx_img.cprate, i);
+            rgbaDst.push_back(arrToMat(out));
         }
         rgbaDst.push_back(rgbaSrc.get(3))
         cv.merge(rgbaDst, dst);
         cv.imshow('after_img', dst);
+        endTime = performance.now();
         src.delete();
         dst.delete();
+        displayPerfStats();
         genDlButtonTruePixel();
     }
 }
